5dae866571078a67d25bb11278a9b067114557aa
blueyorange <blueyorange@gmail.com>
2024-05-31 11:15:16 +0100

    added border tiles

--- src/routes/GameBoardModel.ts
+++ src/routes/GameBoardModel.ts
@@ -24,12 +24,13 @@ export function createGameBoardModel(
 	if (questions.length !== rows * cols) {
 		throw new RangeError('Number of questions does not match size of grid.');
 	}
-	return questions.map((questionData: Question, index): TileModel => {
+	const tiles = questions.map((questionData: Question, index): TileModel => {
 		// use modulo arithmetic to get column number
-		const col = index % cols;
+		// offset columns by 2 to fit border tiles and so odd columns are offset down
+		const col = (index % cols) + 2;
 		// coords stored as doubled row
 		// odd columns get one added to row to offset vertically
-		const row = 2 * Math.floor(index / cols) + (col % 2);
+		const row = 2 * Math.floor(index / cols) + (col % 2) + 2;
 		const state = TileState.Ready;
 		const { question, answer, letter } = questionData;
 		return {
@@ -41,4 +42,37 @@ export function createGameBoardModel(
 			state
 		};
 	});
+	// Blue border tiles (column on both sides)
+	// rows go up in twos
+	for (let row = 1; row <= 2 * rows + 1; row += 2) {
+		tiles.push({ question: '', answer: '', letter: '', col: 1, row: row, state: TileState.Blue });
+		tiles.push({
+			question: '',
+			answer: '',
+			letter: '',
+			col: cols + 2,
+			row: row,
+			state: TileState.Blue
+		});
+	}
+	// white border tiles
+	for (let col = 2; col <= cols + 1; col += 1) {
+		tiles.push({
+			question: '',
+			answer: '',
+			letter: '',
+			col: col,
+			row: col % 2,
+			state: TileState.White
+		});
+		tiles.push({
+			question: '',
+			answer: '',
+			letter: '',
+			col: col,
+			row: (col % 2) + rows * 2 + 2,
+			state: TileState.White
+		});
+	}
+	return tiles;
 }
